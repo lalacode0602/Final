888a79214c7234ce2fa4fc17e7eed40686fe4a7f
Lala Asanuma <lalacode0602@gmail.com>
2022-05-12 19:03:09 -0700

    preparation

---
+++ dblib.js
@@ -0,0 +1,10 @@
+// Add packages
+require("dotenv").config();
+// Add database package and connection string
+const { Pool } = require('pg');
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
