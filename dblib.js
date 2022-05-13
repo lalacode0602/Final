4d3fad72f923a517c3b01c6e923bab5e685cd5b7
Lala Asanuma <lalacode0602@gmail.com>
2022-05-12 21:39:27 -0700

    submit

--- dblib.js
+++ dblib.js
@@ -1,5 +1,6 @@
 // Add packages
 require("dotenv").config();
+const { param } = require("express/lib/request");
 // Add database package and connection string
 const { Pool } = require('pg');
 const pool = new Pool({
@@ -8,3 +9,60 @@ const pool = new Pool({
         rejectUnauthorized: false
     }
 });
+
+const getTotalRecords = () => {
+  sql = "SELECT COUNT(*) FROM book";
+  return pool.query(sql)
+      .then(result => {
+          return {
+              msg: "success",
+              totRecords: result.rows[0].count
+          }
+      })
+      .catch(err => {
+          return {
+              msg: `Error: ${err.message}`
+          }
+      });
+};
+
+const insertBook = (book) => {
+  if (book instanceof Array) {
+      params = book;
+  } else {
+      params = Object.values(book);
+  };
+  // newparams = [];
+  // for(i = 0; i < params.length; i++){
+  //   params.rows[i].forEach(value => {
+  //     if((value).toUpperCase()=== "NULL"){
+  //       val = null;
+  //       newparams.push(val);
+  //     }else{
+  //       newparams.push(value)
+  //     }
+  //   })
+  // }
+  const sql = "INSERT INTO book (book_id, title, total_pages, rating, isbn, published_date) VALUES ($1, $2, $3, $4, $5, $6)";
+  return pool.query(sql, newparams)
+      .then(result => {
+          console.log("inserted");
+          return { 
+              message: "success",
+              desc: `book id ${params[0]} successfully inserted`
+              //result: result.rows
+          }
+      })
+      .catch(err => {
+          return {
+              message: "Error",
+              desc: `Error on insert of book id ${params[0]}.  ${err.message}`
+              //result: `Error: ${err.message}`
+          }
+  });
+}
+
+
+
+module.exports.getTotalRecords = getTotalRecords;
+module.exports.insertBook = insertBook;
--- index.js
+++ index.js
@@ -38,3 +38,91 @@ app.get("/", (req, res) => {
     //res.send("Root resource - Up and running!")
     res.render("index");
 });
+
+app.get("/sum", (req, res) => {
+  const nums = {
+    strnum:"",
+    endnum: "",
+    incr: ""
+  }
+  res.render("sum", {
+    type: "get",
+    nums: nums
+  });
+});
+
+app.post("/sum", (req, res) => {
+  nums = req.body
+  if(nums.strnum > nums.endnum){
+    result = "Starting number must be less than ending number"
+    res.render("sum", {
+      type: "post",
+      nums: nums,
+      message: "error",
+      result: result
+    });
+  }else{
+    const incrnum = () => {
+      result = 0;
+      console.log(nums);
+      for (let i = Number(nums.strnum); i <=Number( nums.endnum); i += Number(nums.incr)) {
+      result += i;
+      console.log("inFunction:", result);
+      }
+      return result
+    }
+  sum = incrnum();
+  res.render("sum", {
+    type: "post",
+    nums: nums,
+    message: "success",
+    result: sum
+  })
+}
+
+})
+
+app.get("/import", async (req, res) => {
+  const totRecs = await dblib.getTotalRecords();
+  message = "";
+  res.render("import", {type: "get", totRecs: totRecs.totRecords, message: message});
+});
+
+app.post("/import",  upload.single('filename'), async(req, res) => {
+  if(!req.file || Object.keys(req.file).length === 0) {
+      message = "Error: Import file not uploaded";
+      res.render("import", {type: "post", errors: message});
+  }else{
+      const totRecs = await dblib.getTotalRecords();
+      //Read file line by line, inserting records
+      const buffer = req.file.buffer; 
+      const lines = buffer.toString().split(/\r?\n/);
+      messages = [];
+      i = 0;
+      r = 0;
+      for(line of lines){
+       //console.log(line);
+          book = line.split(",");
+          console.log(line);
+          const lineinsert = await dblib.insertBook(book);
+          if (lineinsert.message === "success"){
+              i += 1;
+              console.log(lineinsert.desc, i);
+          }else{
+              r += 1;
+              messages.push(lineinsert.desc)
+              console.log(lineinsert.desc, r);
+          }
+      }
+      const totRecs2 = await dblib.getTotalRecords();
+      res.render("import", {
+          type : "post",
+          totRecs: totRecs.totRecords,
+          totRecs2: totRecs2.totRecords, 
+          inserted: i, 
+          notinserted: r,
+          processed: i+r, 
+          errors: messages
+      })
+  }
+  });
